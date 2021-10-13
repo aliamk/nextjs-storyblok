a58ec72b442b01e8f5eff6bb78e024441f39bb24
Alia <malek.alia@gmail.com>
2021-10-13 16:16:47 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,45 @@
-// The Storyblok Client
-import Storyblok from "../lib/storyblok"
-import useStoryblok from "../lib/storyblok-hook"
-import Page from '../components/Page'
-import Layout from '../components/Layout'
+// The Storyblok Client & hook
+import Storyblok, { useStoryblok } from "../lib/storyblok";
+import DynamicComponent from "../components/DynamicComponent";
+import Page from "../components/Page";
+import Layout from "../components/Layout";
 
-export default function Home(props) {
+export default function Home({ story, preview }) {
   // the Storyblok hook to enable live updates
-  const story = useStoryblok(props.story)
+  story = useStoryblok(story, preview);
 
   return (
     <div>
       <main>
-        { story ? <Layout><Page content={story.content} /></Layout> : null }
+        {story ? (
+          <Layout>
+            <Page content={story.content} />
+          </Layout>
+        ) : null}
       </main>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
-  let slug = "home"
+  let slug = "home";
   let params = {
     version: "draft", // or 'published'
-    "resolve_relations": "featured-articles.articles"
-  }
+    resolve_relations: "featured-articles.articles",
+  };
 
   if (context.preview) {
-    params.version = "draft"
-    params.cv = Date.now()
+    params.version = "draft";
+    params.cv = Date.now();
   }
 
-  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params)
+  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params);
 
   return {
     props: {
       story: data ? data.story : false,
-      preview: context.preview || false
+      preview: context.preview || false,
     },
-    revalidate: 10,
-  }
+    revalidate: 3600, // revalidate every hour
+  };
 }
